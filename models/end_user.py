86205e4bb39f164243a254eef1017af75691f9ed
Santoshi <kalaskars1996@gmail.com>
2022-06-02 21:19:21 +0530

    [Santoshi] Modified: API for get & create new end user

--- models/end_user.py
+++ models/end_user.py
@@ -1,9 +1,11 @@
-from pydantic import BaseModel, EmailStr
+from tokenize import Double
+from pydantic import BaseModel, EmailStr, Field
+from fastapi import FastAPI, Query
 from typing import Union
 
 class end_User(BaseModel):
-    name: Union[str, None] = None 
-    email : EmailStr
-    mobile_number: Union[int, None] = None
+    name: str = Field( default="your name", title="Enter your Name", max_length=50 )
+    email : EmailStr = Field( default="your Email Id", title="Enter your mail ID" )
+    mobile_number: int = Field(description="The Mobile number must be 10 digit")
    
    
--- routes/end_user.py
+++ routes/end_user.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 from models.end_user import end_User 
 from config.database import db 
 from schemas.user import serializeList, serializeDict
@@ -7,11 +7,27 @@ from schemas.user import serializeList, serializeDict
 end_user = APIRouter() 
 
 @end_user.get('/')
+def welcome():
+    return {"detail":"Welcome to Bridgelabz Chatbot program..!!!"}
+
+
+@end_user.get('/end_user', status_code=status.HTTP_200_OK)
 async def find_all_users():
+    data = serializeList(db.end_User.find())
+    if not data:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="End Users data not Available!!!")
     return serializeList(db.end_User.find())
 
-@end_user.post('/')
-async def create_user(user: end_User):
-        obj = db.end_User.insert_one(dict(user))
-        return serializeDict(db.end_User.find())
+
+@end_user.post('/end_user', status_code=status.HTTP_200_OK, response_model=end_User)
+async def create_new_user(user: end_User):
+        existing_user = db.end_User.find_one({"email": user.email})
+        if existing_user is not None:
+            raise HTTPException(status_code=404, 
+                                detail= {"data": f"User with {user.email} is already Exist",
+                                        "Object Id": str(existing_user["_id"])})
+        new_user = db.end_User.insert_one(dict(user))
+        if not new_user:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid input data!!!")
+        return user
 
